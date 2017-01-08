29d904ac5d8a64de1fe3542e93018ce92e196eea
hville <hugov@runbox.com>
2017-01-08 10:28:12 +0800

    named risks subset of all risks

--- correl-z.js
+++ correl-z.js
@@ -9,25 +9,28 @@ module.exports = correlZ
  * @returns {Function} - simulator
  */
 function correlZ(iidWeights) {
-	const intW = getInternalWeight(iidWeights)
+	const iids = keyVal(iidWeights),
+				intW = getInternalWeight(iids)
 	if (Number.isNaN(intW)) throw Error('sum of the iid factors squares must be less than 1')
 
 	return function random(iidZs, selfZ) {
 		let res = (selfZ === undefined ? rndZ() : selfZ) * intW
-		for (let i=0; i<iidWeights.length; ++i) {
-			if (iidWeights[i] !== 0) res += iidZs[i] * iidWeights[i]
-		}
+		for (let i=0; i<iids.length; ++i) res += iidZs[iids[i][0]] * iids[i][1]
 		if (isNaN(res)) throw Error('invalid risk weights or seed values')
 		return res
 	}
 }
-function getInternalWeight(extW) {
+function getInternalWeight(iids) {
 	let t = 1
-	for (let i=0; i<extW.length; ++i) {
-		if (extW[i] !==0 ) t -= extW[i] * extW[i]
-	}
+	for (let i=0; i<iids.length; ++i) t -= iids[i][1] * iids[i][1]
 	// recover from float point error if very near 0
 	if (t > 0) return Math.sqrt(t)
 	if (t > -(Number.EPSILON || 2.22045E-16)) return 0
 	return NaN
+}
+function keyVal(obj) {
+	const ks = Object.keys(obj),
+				res = []
+	for (let i=0; i<ks.length; ++i) if (obj[ks[i]] !== 0) res.push([ks[i], obj[ks[i]]])
+	return res
 }
--- test.js
+++ test.js
@@ -11,10 +11,13 @@ var N = 5000
 function mapFcn(f) { return f(this) } /*eslint no-invalid-this: 0*/
 
 ct('mutually independent', () => {
-	var rnds = [ CZ([1,0]), CZ([0,1]) ],
-			stats = LS(rnds.length)
-	for (var i=0; i<N; ++i) {
-		var zs = rnds.map(mapFcn, rnds.map(rndZ))
+	const rnds = [],
+				stats = LS(2)
+	ct('===', rnds.push(CZ([1,0])), 1)
+	ct('===', rnds.push(CZ([0,1])), 2)
+
+	for (let i=0; i<N; ++i) {
+		let zs = rnds.map(mapFcn, rnds.map(rndZ))
 		stats.push(zs)
 	}
 	ct('<', Math.abs(stats.ave(0)), 0.05)
@@ -22,11 +25,11 @@ ct('mutually independent', () => {
 	ct('<', Math.abs(stats.cor(0, 1)), 0.05)
 })
 ct('mutually dependent', () => {
-	var rnds = [ CZ([0.8,0.6]), CZ([0.8,0.6]) ],
-			stats = LS(rnds.length)
+	const rnds = [ CZ([0.8,0.6]), CZ([0.8,0.6]) ],
+				stats = LS(rnds.length)
 
-	for (var i=0; i<N; ++i) {
-		var zs = rnds.map(mapFcn, rnds.map(rndZ))
+	for (let i=0; i<N; ++i) {
+		let zs = rnds.map(mapFcn, rnds.map(rndZ))
 		stats.push(zs)
 	}
 
@@ -35,12 +38,12 @@ ct('mutually dependent', () => {
 	ct('<', Math.abs(stats.cor(0, 1)-1), 0.05)
 })
 ct('target correlation', () => {
-	var rho = 0.5,
-			rnds = [ CZ([1]), CZ(cho([[1, rho],[rho,1]])[1]) ],
-			stats = LS(rnds.length)
+	const rho = 0.5,
+				rnds = [ CZ([1]), CZ(cho([[1, rho],[rho,1]])[1]) ],
+				stats = LS(rnds.length)
 
-	for (var i=0; i<N; ++i) {
-		var zs = rnds.map(mapFcn, rnds.map(rndZ))
+	for (let i=0; i<N; ++i) {
+		let zs = rnds.map(mapFcn, rnds.map(rndZ))
 		stats.push(zs)
 	}
 
@@ -48,8 +51,17 @@ ct('target correlation', () => {
 	ct('<', Math.abs(stats.var(0)-1), 0.05)
 	ct('<', Math.abs(stats.cor(0, 1)-rho), 0.05)
 })
-//console.log('test self correlation...')
-//console.log('test cross correlation...')
-//closeTo(stats.cor(1,2), r1*r2, 0.05, 'should be near the target correlation')
-//console.log('test cross correlation with multiple factors...')
-//console.log('testing cross correlation with multiple negative factors...')
+ct('mutually independent - named risks', () => {
+	const rnds = [],
+				stats = LS(2)
+	ct('===', rnds.push(CZ({0:1})), 1)
+	ct('===', rnds.push(CZ({1:1})), 2)
+
+	for (let i=0; i<N; ++i) {
+		let zs = rnds.map(mapFcn, rnds.map(rndZ))
+		stats.push(zs)
+	}
+	ct('<', Math.abs(stats.ave(0)), 0.05)
+	ct('<', Math.abs(stats.var(0)-1), 0.05)
+	ct('<', Math.abs(stats.cor(0, 1)), 0.05)
+})
